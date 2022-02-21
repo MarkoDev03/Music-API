2a70db3ad0b5c8bce332657a307bc783652d0534
MarkoDev03 <mmarko.perovici3@gmail.com>
2022-02-21 10:33:29 +0100

    make it better 12

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ mongoose.connect(process.env.DB_URI, {
     console.log(error) 
 })
 
-app.listen(process.env.PORT, () => {
+let port = process.env.PORT || 5001
+
+app.listen(port, () => {
     console.log("App is alive!")
 })
 
